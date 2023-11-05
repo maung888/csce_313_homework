8f67c00599e9cae99d906ad67856120a5705c043
maung888 <maung@tamu.edu>
2023-11-05 23:05:31 +0000

    v3

--- openr.s
+++ openr.s
@@ -2,17 +2,7 @@
 .type openr, @function
 
 openr:
-    # System call number for open
     movq $2, %rax  # sys_open
-
-    # First argument: pathname (already in %rdi)
-    # No need to move it.
-
-    # Second argument: flags
     movq $0, %rsi  # O_RDONLY
-
-    # Invoke syscall
     syscall
-
-    # Return the result
     ret
